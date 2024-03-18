4f1ab603784d26cd8244c918c30f0fc69e60c9e2
chiragyadav916 <chiragyadav916@gmail.com>
2024-03-18 23:10:23 +0530

    18March'24

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const Listing = require("./models/listing.js")
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js")
+const { listingSchema } = require("./schema.js");
 
 const MONGO_URl = "mongodb://127.0.0.1:27017/aasaan_aavaas";
 async function main() {
@@ -31,10 +34,10 @@ app.get("/", (req, res) => {
 });
 
 //Index Route
-app.get("/listings", async (req, res) => {
+app.get("/listings", wrapAsync(async (req, res) => {
     const allListings = await Listing.find({});
     res.render("listings/index.ejs", { allListings });
-});
+}));
 
 //New Route
 app.get("/listings/new", (req, res) => {
@@ -42,41 +45,64 @@ app.get("/listings/new", (req, res) => {
 });
 
 //Show Route
-app.get("/listings/:id", async (req, res) => {
+app.get("/listings/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/show.ejs", { listing });
-});
+}));
 
 //Create Route
-app.post("/listings", async (req, res) => {
+app.post("/listings", wrapAsync(async (req, res) => {
     //let listing = req.body;//data is in JS object 
+
+    // if (!req.body.listing) {
+    //     throw new ExpressError(400, "Send valid data for listing");
+    // }
+
+    // let result=listingSchema.validate(req.body);
+    // console.log(result);
     let listing = req.body.listing;
     const newListing = new Listing(listing);//it gives an instance
     await newListing.save();
     res.redirect("/listings");
-});
+}));
 
 //Edit Route 
-app.get("/listings/:id/edit", async (req, res) => {
+app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("listings/edit.ejs", { listing });
-});
+}));
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
+    if (!req.body.listing) {
+        throw new ExpressError(400, "Send valid data for listing");
+    }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });//deconstructing
     res.redirect(`/listings/${id}`);
-});
+}));
 
 //Delete Route
-app.delete("/listings/:id/", async (req, res) => {
+app.delete("/listings/:id/", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deleteHotel = await Listing.findByIdAndDelete(id);
     console.log(deleteHotel);
     res.redirect(`/listings`);
+}));
+
+//If Route not match
+app.all("*", (req, res, next) => {
+    next(new ExpressError(404, 'Page Not Found!'));
+});
+
+//Custom Error Handlers
+app.use((err, req, res, next) => {
+    let { statusCode = 500, message = "Something Went Wrong!" } = err;//default value
+    res.status(statusCode).render("error.ejs", { message });
+    //res.status(statusCode).send(message);
+    // res.send("Something went wrong!");
 });
 
 // app.get("/testListing", async (req, res) => {
