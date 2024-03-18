016eb753aee9c0f37839fb970be4f4deeaa9680d
chiragyadav916 <chiragyadav916@gmail.com>
2024-03-19 00:46:05 +0530

    19March'24

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js")
-const { listingSchema } = require("./schema.js");
+// const { listingSchema } = require("./schema.js");
 
 const MONGO_URl = "mongodb://127.0.0.1:27017/aasaan_aavaas";
 async function main() {
@@ -61,8 +61,14 @@ app.post("/listings", wrapAsync(async (req, res) => {
 
     // let result=listingSchema.validate(req.body);
     // console.log(result);
-    let listing = req.body.listing;
-    const newListing = new Listing(listing);//it gives an instance
+
+    // let listing = req.body.listing;
+    // const newListing = new Listing(listing);//it gives an instance
+
+    if (!req.body.listing) {
+        throw new ExpressError(400, "Send valid data for listing");//custom error message
+    }
+    const newListing = new Listing(req.body.listing);//it gives an instance
     await newListing.save();
     res.redirect("/listings");
 }));
@@ -77,7 +83,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 //Update Route
 app.put("/listings/:id", wrapAsync(async (req, res) => {
     if (!req.body.listing) {
-        throw new ExpressError(400, "Send valid data for listing");
+        throw new ExpressError(400, "Send valid data for updation");
     }
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, { ...req.body.listing });//deconstructing
@@ -101,7 +107,8 @@ app.all("*", (req, res, next) => {
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something Went Wrong!" } = err;//default value
     res.status(statusCode).render("error.ejs", { message });
-    //res.status(statusCode).send(message);
+
+    // res.status(statusCode).send(message);
     // res.send("Something went wrong!");
 });
 
